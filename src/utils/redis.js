c45037a585a86bcc24f109319615ba434d4e1e53
lichun <lchrennew@126.com>
2022-02-18 10:14:03 +0800

    opt: migrate to es-ioredis-url

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,30 +1,3 @@
-import RedisURL from './redis-url.js';
-import { getLogger } from "koa-es-template";
+import { RedisURL } from "es-ioredis-url";
 
-const logger = getLogger('src/utils/redis.js')
-
-/**
- *
- * @type {Redis}
- */
 export const redis = new RedisURL().getRedis()
-export const initRedis = async () => {
-    if (redis) {
-        await redis.set('ready', 'ready')
-        logger.info(`Redis connection: ${await redis.get('ready')}`)
-    }
-}
-
-export const hscanAll = async (key, { match, count }) => {
-    let cursor = '0'
-    const result = {}
-    do {
-        const [ nextCursor, values ] = await redis.hscan(key, cursor, ...(match ? [ 'MATCH', match ] : []), ...(count ? [ 'COUNT', count ] : []))
-        cursor = nextCursor
-        console.log(cursor)
-        for (let i = 0; i < values.length; i++) {
-            result[values[i]] = values[++i]
-        }
-    } while (cursor !== '0')
-    return result
-}
